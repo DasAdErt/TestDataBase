0eea5114c7cf214516bee3bb7c595433efcfef55
DasAdErt <misha_tkachev_2018@mail.ru>
2023-09-15 16:54:15 +0300

    Edited file User.java

--- src/RegistrationService.java
+++ src/RegistrationService.java
@@ -1,10 +1,36 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RegistrationService {
+
+    public boolean isLoginUnique(String login) {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            String sql = "SELECT COUNT(*) FROM users WHERE login = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, login);
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        int count = resultSet.getInt(1);
+                        return count == 0;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     public boolean registerUser(String login, String email, String password, String secretKey) {
         try (Connection connection = DatabaseConnection.getConnection()) {
+            // Проверяем уникальность логина
+            if (!isLoginUnique(login)) {
+                System.out.println("\nПользователь с таким логином уже существует.");
+                return false;
+            }
+
             String sql = "INSERT INTO users (login, email, password, secret_key) VALUES (?, ?, ?, ?)";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 preparedStatement.setString(1, login);
@@ -20,5 +46,4 @@ public class RegistrationService {
             return false;
         }
     }
-
 }
--- src/User.java
+++ src/User.java
@@ -6,16 +6,17 @@ public class User {
     Scanner scanner = new Scanner(System.in);
 
     private void choice(){
-        System.out.print("Выберите действие:\n1.Войти\n2.Зарегистрироваться\nВаш выбор: ");
+        System.out.print("Выберите действие:\n1. Войти\n2. Зарегистрироваться\nВаш выбор: ");
         String choiceSign = scanner.nextLine();
 
         switch (choiceSign){
             case "1" -> {
-                registration();
-            }
-            case "2" -> {
                 System.out.println("\n\n\nВы выбрали вход в существующий аккаунт!");
                 login();
+            }
+            case "2" -> {
+                System.out.println("\n\n\nВы выбрали регистрацию аккаунта!");
+                registration();
             }
             default -> System.out.println("Вы не прошли проверку на IQ!");
         }
@@ -24,6 +25,11 @@ public class User {
     private void registration(){
         System.out.print("Введите логин: ");
         String login = scanner.nextLine();
+
+        if (!registrationService.isLoginUnique(login)) {
+            System.out.println("\nПользователь с таким логином уже существует.");
+            return;
+        }
 
         System.out.print("Введите вашу почту: ");
         String email = scanner.nextLine();
@@ -38,12 +44,9 @@ public class User {
 
         if (registrationSuccessful) {
             System.out.println("Регистрация прошла успешно!");
-        } else {
-            System.out.println("Ошибка при регистрации. Пожалуйста, проверьте данные.");
+            System.out.println("\n\nВойдите в только что созданный аккаунт!");
+            login();
         }
-
-        System.out.println("\n\nВойдите в только что созданный аккаунт!");
-        login();
     }
 
     private void login(){
@@ -60,16 +63,119 @@ public class User {
         if (loginService.login(usernameOrMail, password)) {
             if (isAdmin){
                 System.out.println("Вы вошли в профиль Админа!");
+                userProfileMenu(usernameOrMail, true);
             } else {
                 System.out.println("Вход выполнен успешно!");
+                userProfileMenu(usernameOrMail, false);
             }
         } else {
             System.out.println("Ошибка входа. Проверьте имя пользователя и пароль.");
         }
     }
 
-    private void changeDataUser(){
+    private void userProfileMenu(String login, boolean isAdmin) {
+        System.out.println("\n\n\nДобро пожаловать в меню профиля, " + login + "!");
+
+        if (isAdmin) { // Выбор для админа
+            System.out.println("Выберите меню: \n1. Меню Администратора\n2. Меню пользователя\nВыбор: ");
+
+            int choiceMenus = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choiceMenus){
+                case 1 -> { //Меню Администратора
+                    System.out.print("Меню для администратора:\n1. Посмотреть БД\n" +
+                            "2. Сделать запрос в БД\n3. Выход\nВаш выбор: ");
+
+                    int adminChoice = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch (adminChoice) {
+                        case 1:
+                            viewDatabaseTable();
+                            break;
+                        case 2:
+                            databaseQueryMenu();
+                            break;
+                        case 3:
+                            System.exit(0);
+                            break;
+                        default:
+                            System.out.println("Неверный выбор.");
+                            break;
+                    }
+                }
+                case 2 -> { // Меню пользователя в админке
+                    System.out.print("Меню пользователя:\n1. Об аккаунте\n2. Поменять почту\n" +
+                            "3. Поменять пароль\n4. Выход\nВыбор: ");
+
+                    int userChoice = scanner.nextInt();
+
+                    scanner.nextLine();
+
+                    switch (userChoice) {
+                        case 1:
+                            viewAccountInfo(login);
+                            break;
+                        case 2:
+                            changeEmail(login);
+                            break;
+                        case 3:
+                            changePassword(login);
+                            break;
+                        case 4:
+                            System.exit(0);
+                            break;
+                        default:
+                            System.out.println("Ошибка, как так то?");
+                            break;
+                    }
+                }
+                default -> System.out.println("Ошибка, так получилось!");
+            }
+        } else { // Меню пользователя без админки
+            System.out.print("Меню пользователя:\n1. Об аккаунте\n2. Поменять почту\n" +
+                    "3. Поменять пароль\n4. Выход\nВыбор: ");
+
+            int userChoice = scanner.nextInt();
+
+            scanner.nextLine();
+
+            switch (userChoice) {
+                case 1:
+                    viewAccountInfo(login);
+                    break;
+                case 2:
+                    changeEmail(login);
+                    break;
+                case 3:
+                    changePassword(login);
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Это перебор уже, хватит!");
+                    break;
+            }
+        }
     }
+
+    private void viewAccountInfo(String login) {
+    }
+
+    private void changeEmail(String login) {
+    }
+
+    private void changePassword(String login) {
+    }
+
+    private void viewDatabaseTable() {
+    }
+
+    private void databaseQueryMenu() {
+    }
+
 
     public void run(){
         choice();
